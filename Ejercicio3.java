728ca3ab360a68cbb0736e4c75f51a6a76be4b15
Jeanpx20 <143896852+Jeanpx20@users.noreply.github.com>
2023-09-02 14:00:20 -0500

    Create Ejercicio3.java

---
+++ Ejercicio3.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int numA = 7; // cambiamos el valor
+        int numB = 5; // cambiamos el valor
+
+        if (numA > 0 && numB > 0) {
+            System.out.println("true");
+        } else {
+            System.out.println("false");
+        }
+    }
+}
