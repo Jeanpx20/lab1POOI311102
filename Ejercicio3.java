ba71d8543b922b887fde0a719fbd85627b44194b
Jeanpx20 <143896852+Jeanpx20@users.noreply.github.com>
2023-09-02 14:02:41 -0500

    Update Ejercicio3.java

--- Ejercicio3.java
+++ Ejercicio3.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        int numA = 7; // cambiamos el valor
-        int numB = 5; // cambiamos el valor
+        int numA = 15; // cambiamos el valor
+        int numB = 8; // cambiamos el valor
 
         if (numA > 0 && numB > 0) {
             System.out.println("true");
